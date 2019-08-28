85ce0b6d05ce26b6a582d4fe8604b750706bfc02
xuming <xuming@ef5c88b3-4516-9443-91c8-fa497ed12240>
2019-08-28 05:43:01 +0000

    git-svn-id: svn://192.168.2.3/App/JSRT/trunk/src/webapp@59339 ef5c88b3-4516-9443-91c8-fa497ed12240

--- src/DS/Model/index.ts
+++ src/DS/Model/index.ts
@@ -11,9 +11,9 @@ import News from './news';
 import RunTime from '@/DS/util';
 
 // 引入server_config
- const server_config = require('../../../server_config');
- let apiAddress = server_config.serverSettingsUrl;
-// let apiAddress = RunTime.getUrl();
+// const server_config = require('../../../server_config');
+// let apiAddress = server_config.serverSettingsUrl;
+ let apiAddress = RunTime.getUrl();
 
 export default {
   Base: new BaseModel(),
