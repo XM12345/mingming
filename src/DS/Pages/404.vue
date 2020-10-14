030f3396508f84aa7b694857e39c9116666cac2e
xuming <xuming@ef5c88b3-4516-9443-91c8-fa497ed12240>
2020-10-14 06:11:31 +0000

    优化|新增404页面
    git-svn-id: svn://192.168.2.3/App/JSRT/trunk/src/webapp@73750 ef5c88b3-4516-9443-91c8-fa497ed12240

--- src/DS/Pages/404.vue
+++ src/DS/Pages/404.vue
@@ -1,5 +1,30 @@
 <template>
-    <div class="page-404">
-        404
+  <div class="page-404">
+    <div class="page-404-main">
+      <img src="../Components/base/_images/404.png" />
+      <p>哎呀，这个页面一边凉快去了</p>
     </div>
+  </div>
 </template>
+
+<style lang="scss">
+.page-404 {
+  &-main {
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    & > img {
+      background-color: #fff;
+      max-width: 50%;
+    }
+    & > p {
+      text-align: center;
+      font-size: 17px;
+      color: #666;
+    }
+  }
+}
+</style>
