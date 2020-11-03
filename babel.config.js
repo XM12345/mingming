f61f4408f06063e53d92a4f38edaa686a0d0753c
xuming <xuming@ef5c88b3-4516-9443-91c8-fa497ed12240>
2020-11-03 06:20:40 +0000

    优化|基础配置文件
    git-svn-id: svn://192.168.2.3/App/JSRT/trunk/src/webapp@74321 ef5c88b3-4516-9443-91c8-fa497ed12240

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  presets: ['@vue/app'],
-  plugins: [['component', { libraryName: 'mint-ui', style: true }]]
+  presets: [
+    [
+      '@vue/app',
+      { polyfills: ['es6.array.iterator', 'es6.promise', 'es6.object.assign', 'es7.promise.finally', 'es6.symbol'] }
+    ]
+  ],
+  plugins: [
+    ['component', { libraryName: 'mint-ui', style: true }],
+    ['import', { libraryName: 'vant', libraryDirectory: 'es', style: true }, 'vant'],
+    ['@babel/plugin-proposal-optional-chaining']
+  ]
 };
